314bd14bc81c69f17721a50e7cebd21670813622
Freddy <ftorres0448@gmail.com>
2025-03-20 17:05:36 -0500

    more

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
+
 from api_client import ApiClient
 
 api_client = ApiClient("https://linkedin-data-api.p.rapidapi.com")
-response = api_client.search_jobs("search-jobs?keywords=golang&locationId=92000000&datePosted=anyTime&sort=mostRelevant", {'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com', 'x-rapidapi-key': '959ffaad04msh195f863bae0c884p151c0bjsn64bc8e40f8b1'})
+response = api_client.search_jobs("search-jobs?keywords=software%20engineer&locationId=103644278&datePosted=past24Hours&sort=mostRecent", {'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com', 'x-rapidapi-key': '959ffaad04msh195f863bae0c884p151c0bjsn64bc8e40f8b1'})
 data = response.json()
-print(data)
+print(json.dumps(data, indent=4))
