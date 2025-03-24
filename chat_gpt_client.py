55ad6b2fa102935e4636fc9d129a2f7fd812ff82
Freddy <ftorres0448@gmail.com>
2025-03-24 16:53:45 -0500

    more

--- chat_gpt_client.py
+++ chat_gpt_client.py
@@ -1,19 +1,25 @@
 from openai import OpenAI
 import yaml
 
-with open('application.yaml', 'r') as file:
-    yamlFields = yaml.full_load(file)
+class ChatGptClient:
 
-client = OpenAI(api_key=yamlFields.get("openAIKey"))
+    def __init__(self):
+        with open('application.yaml', 'r') as file:
+            yamlFields = yaml.full_load(file)
 
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {
-            "role": "user",
-            "content" : "Write a one-sentence bedtime story about a unicorn."
-        }
-    ]
-)
+        self.client = OpenAI(api_key=yamlFields.get("openAIKey"))
 
-print(completion.choices[0].message.content)
+    def make_call(self):
+        promptString = "I am a software engineer with 3 years of experience. I have a Bachelor's degree in Computer Engineering, and am proficient in Java, Python, C++, and Swift. Would I be a strong applicant for job with this description? "
+
+        completion = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "user",
+                    "content": promptString
+                }
+            ]
+        )
+
+        print(completion.choices[0].message.content)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 
 from api_client import ApiClient
+from chat_gpt_client import ChatGptClient
 
 api_client = ApiClient("https://linkedin-data-api.p.rapidapi.com")
 response = api_client.search_jobs("search-jobs?keywords=software%20engineer&locationId=104472865&datePosted=past24Hours&sort=mostRecent", {'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com', 'x-rapidapi-key': '959ffaad04msh195f863bae0c884p151c0bjsn64bc8e40f8b1'})
 data = response.json()
 
-for x in range(len(data["data"])):
-    print(data["data"][x]["id"])
+ChatGptClient().make_call()
